6cc5b2a9ea097ad88be5895b676a256ae3ed88ef
Goongam <dlwjddn341@naver.com>
2023-08-05 12:32:02 +0900

    Feat: 프로젝트 이름 변경

--- ios/DrawWakeTests/DrawWakeTests.m
+++ ios/DrawWakeTests/DrawWakeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AlarmApp2Tests : XCTestCase
+@interface DrawWakeTests : XCTestCase
 
 @end
 
-@implementation AlarmApp2Tests
+@implementation DrawWakeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
